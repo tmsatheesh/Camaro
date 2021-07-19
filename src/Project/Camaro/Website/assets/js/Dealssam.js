255fdd6e449d46fc8fd8f104c3f84ace28c13c9c
tmsatheesh <87616833+tmsatheesh@users.noreply.github.com>
2021-07-19 11:40:20 +0530

    Commit

---
+++ src/Project/Camaro/Website/assets/js/Dealssam.js
@@ -0,0 +1,29 @@
+﻿function LoadDealssam() {
+    $.ajax({
+        //type: 'GET',
+        //url: '/api/sitecore/DealsSam/GetDealsSam?settingsid=' + $('#deallist').data('settingsid')
+        //    + '&categoryid=' + $('#category').val() + '&subcategoryid=' + $('#subcategory').val()
+        //    + '&searchlocation=' + $('#Location').val() + '&searchtitle=' + $('#searchtitle').val(), 
+
+        type: 'POST',
+        url: '/api/sitecore/DealsSam/GetDealsPost',
+        data: {
+            "settingsid": $('#deallist').data('settingsid'),
+            "categoryid": $('#category').val(),
+            "subcategoryid": $('#subcategory').val(),
+            "searchlocation": $('#Location').val(),
+            "searchtitle": $('#searchtitle').val(),
+
+        },           
+        success: function (data) {
+            UpdateDealsTemplate(data);
+        }
+    });
+}
+function UpdateDealsTemplate(data) {
+    var template = document.getElementById('Deals-template').innerHTML;
+    var templateScript = Handlebars.compile(template);
+    var context = data;
+    var html = templateScript(context);
+    document.getElementById('deallist').innerHTML = html;
+}
---
+++ src/Project/Camaro/Website/assets/js/deals.js
@@ -0,0 +1,34 @@
+﻿
+function LoadDeals() {
+    //$.ajax({
+    //    type: 'GET',
+    //    url: '/api/sitecore/Deals/GetDeals?settingsid=' + $('#deallist').data('settingsid')
+    //        + '&categoryid=' + $('#category').val() + "&subcategoryid=" + $('#subcategory').val()
+    //        + '&SearchQuery=' + $('#searchquery').val(),
+    //    success: function (data) {
+    //        UpdateDealsTemplate(data);
+    //    }
+    //});
+    $.ajax({
+        type: 'POST',
+        url: '/api/sitecore/Deals/GetPostDeals',
+        data: {
+            "settingsid": $('#deallist').data('settingsid'),
+            "categoryid": $('#category').val(),
+            "subcategoryid": $('#subcategory').val(),
+            "SearchQuery": $('#searchquery').val(),
+
+        },
+        
+        success: function (data) {
+            UpdateDealsTemplate(data);
+        }
+    });
+}
+function UpdateDealsTemplate(data) {
+    var template = document.getElementById('Deals-template').innerHTML;
+    var templateScript = Handlebars.compile(template);
+    var context = data;
+    var html = templateScript(context);
+    document.getElementById('deallist').innerHTML = html;
+}
