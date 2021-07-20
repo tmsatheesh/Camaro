769b41dd080f39865a506200fe5a53a5a02066b3
tmsatheesh <87616833+tmsatheesh@users.noreply.github.com>
2021-07-20 22:54:50 +0530

    Banner Updated

---
+++ src/Feature/Components/Website/Controllers/BannerController.cs
@@ -0,0 +1,30 @@
+﻿using Camaro.Feature.Components.Models;
+using Camaro.Foundation.Content.Services;
+using Sitecore.Data;
+using Sitecore.Data.Fields;
+using Sitecore.Mvc.Controllers;
+using Sitecore.Mvc.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Camaro.Feature.Components.Controllers
+{
+    public class BannerController : SitecoreController
+    {
+        private readonly IBaseService _baseService;
+        public BannerController(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+        // GET: Banner
+        public ActionResult Banner()
+        {
+            var datasourceitem = _baseService.GetDataSourceItem<IBanners>();
+            
+            return View("~/Views/Camaro/Components/Banner.cshtml", datasourceitem);
+        }
+    }
+}
