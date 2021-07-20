2cf8b68e5f0c7c1c788fd214dd792c4d52ffcca8
tmsatheesh <87616833+tmsatheesh@users.noreply.github.com>
2021-07-20 17:38:59 +0530

    Added Footer

---
+++ src/Feature/Components/Website/Controllers/FooterController.cs
@@ -0,0 +1,30 @@
+﻿using Camaro.Feature.Components.Models;
+using Camaro.Foundation.Content.Services;
+using Sitecore.Data;
+using Sitecore.Mvc.Controllers;
+using Sitecore.Mvc.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Camaro.Feature.Components.Controllers
+{
+    public class FooterController : SitecoreController
+    {
+        private readonly IBaseService _baseService;
+        public FooterController(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+        
+
+        // GET: Footer
+        public ActionResult Footer()
+        {
+            var datasource = _baseService.GetDataSourceItem<IFooter>();
+            return View("~/Views/Camaro/Components/Footer/Footer.cshtml",datasource);
+        }
+    }
+}
---
+++ src/Feature/Components/Website/Models/Footer.cs
@@ -0,0 +1,67 @@
+﻿using Camaro.Foundation.Common.ORM.Models;
+using Glass.Mapper.Sc.Configuration.Attributes;
+using Glass.Mapper.Sc.Fields;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Camaro.Feature.Components.Models
+{
+    public interface IFooter: IGlassBase
+    {
+        [SitecoreField(FieldName = "Logo")]
+        Image Logo { get; set; }
+        [SitecoreField(FieldName = "Logo Link")]
+        Link LogoLink { get; set; }
+        [SitecoreField(FieldName = "Logo Description")]
+        string LogoDescription { get; set; }
+        [SitecoreField(FieldName = "FB Link")]
+        Link fbLink { get; set; }
+        [SitecoreField(FieldName = "TW Link")]
+        Link twLink { get; set; }
+        [SitecoreField(FieldName = "Vimeo Link")]
+        Link vmLink { get; set; }
+        [SitecoreField(FieldName = "Linkedin Link")]
+        Link lnLink { get; set; }
+        [SitecoreField(FieldName = "Title Service")]
+        string TitleService { get; set; }
+        [SitecoreField(FieldName = "Link Items")]
+        IEnumerable<ILinkItem> linkItems { get; set; }
+        [SitecoreField(FieldName = "Title")]
+        string Title { get; set; }
+        [SitecoreField(FieldName = "Image Link Items")]
+        IEnumerable<IImageLinkItems> imageLinks { get; set; }
+        [SitecoreField(FieldName = "Title News")]
+        string TitleNews { get; set; }
+        [SitecoreField(FieldName = "Description News")]
+        string DescripNews { get; set; }
+        [SitecoreField(FieldName = "Email")]
+        string Email { get; set; }
+        [SitecoreField(FieldName = "Button")]
+        string Button { get; set; }
+        [SitecoreField(FieldName = "Company Link")]
+        Link CompanyLink { get; set; }
+        [SitecoreField(FieldName = "Message")]
+        
+        
+        string Message { get; set; }
+        
+
+
+    }
+    public interface ILinkItem:IGlassBase
+    {
+        [SitecoreField(FieldName = "Linking Items")]
+        Link LinkingItems { get; set; }
+        //[SitecoreField(FieldName = "Email Link")]
+        //string LinkingText { get; set; }
+    }
+    public interface IImageLinkItems:IGlassBase
+    {
+        [SitecoreField(FieldName = "Image")]
+        Image Image { get; set; }
+        [SitecoreField(FieldName = "Image Link")]
+        Link ImageLink { get; set; }
+    }
+}
